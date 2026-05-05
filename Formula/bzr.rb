974789b1a504cdb3b8159e91c2df8d5c095659e4
David Christensen <randomparity@gmail.com>
2026-05-04 19:15:41 -0700

    bzr 0.2.0

--- Formula/bzr.rb
+++ Formula/bzr.rb
@@ -2,29 +2,29 @@ class Bzr < Formula
   desc "CLI for Bugzilla, inspired by gh"
   homepage "https://github.com/randomparity/bzr"
   license "MIT"
-  version "0.2.0-rc3"
+  version "0.2.0"
 
   on_macos do
     on_arm do
-      url "https://github.com/randomparity/bzr/releases/download/v0.2.0-rc3/bzr-v0.2.0-rc3-aarch64-apple-darwin.tar.gz"
-      sha256 "81d61eb7659fd80180008f6cb3f6460aba5d0eaa5b41f2b0f2f2f6f8172c6b43"
+      url "https://github.com/randomparity/bzr/releases/download/v0.2.0/bzr-v0.2.0-aarch64-apple-darwin.tar.gz"
+      sha256 "4256ada2a7a33cab096ea947f1ec6ec4e2c4e107d0170426bf728fe3ba9a6291"
     end
     on_intel do
       # No prebuilt Intel macOS binary — fall back to a source build.
-      url "https://github.com/randomparity/bzr/archive/refs/tags/v0.2.0-rc3.tar.gz"
-      sha256 "ad966f010a7ba4a0d82fa325f21047d81ade438002720f3ea24a2c27abf4f3fa"
+      url "https://github.com/randomparity/bzr/archive/refs/tags/v0.2.0.tar.gz"
+      sha256 "3c653b8f1ba43b7a164fe08e895b6b254fbee9aa758f6ee5db60b27269259774"
       depends_on "rust" => :build
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/randomparity/bzr/releases/download/v0.2.0-rc3/bzr-v0.2.0-rc3-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "b5579341881d78074e012d2613ae636e10fdcb0bf11d35bba093b4dc8e7a6cba"
+      url "https://github.com/randomparity/bzr/releases/download/v0.2.0/bzr-v0.2.0-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "f8d6026ce59016e217a4bdf3977e002a0823c95b872b5c5698098fc96c6f31c0"
     end
     on_intel do
-      url "https://github.com/randomparity/bzr/releases/download/v0.2.0-rc3/bzr-v0.2.0-rc3-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "9a9331865daf6966c4e322b6145d849a0934d06930869df6489ed832e40cca55"
+      url "https://github.com/randomparity/bzr/releases/download/v0.2.0/bzr-v0.2.0-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "9c2d97879aa134c473355984905cfd5209de9c12c222809b46bf8857e32943fe"
     end
   end
 
